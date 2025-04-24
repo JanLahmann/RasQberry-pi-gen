d387cdde74b06086cc1f71e3d0766eb78db86c05
JanLahmann <jan@lahmann-online.de>
2025-04-24 11:15:06 +0200

    rename stage5 to RQB2

--- stage-RQB2/01-install-qiskit/00-run-chroot.sh
+++ stage-RQB2/01-install-qiskit/00-run-chroot.sh
@@ -34,14 +34,16 @@ chmod 755 /home/${FIRST_USER_NAME}/.local/bin
 chmod 755 /home/${FIRST_USER_NAME}/${RQB2_CONFDIR}
 
 # apply RQB2 patch to /usr/bin/raspi-config at boot time
-echo "modify crontab 1"
-crontab -l || true
-(crontab -l 2>/dev/null; echo "@reboot /usr/bin/rq_patch_raspiconfig.sh") | crontab -
-crontab -l || true
+# adding patch script to root-crontab 
+# (could be done more elegantly with crontab command instead of 
+#echo "modify crontab 1"
+#crontab -l || true
+#(crontab -l 2>/dev/null; echo "@reboot /usr/bin/rq_patch_raspiconfig.sh") | crontab -
+#crontab -l || true
 echo "modify crontab 2"
 echo "@reboot /usr/bin/rq_patch_raspiconfig.sh" >> /var/spool/cron/crontabs/root
-crontab -l || true
-echo "end modify crontab 2"
+#crontab -l || true
+#echo "end modify crontab 2"
 
 # Clean up the temporary clone directory if needed
 # Install Qiskit using pip
